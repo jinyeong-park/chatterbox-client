5d8b30fb810ab5bf5797fdfc0976129a8cfd73ef
bbli1012 <ben@onetechnology.io>
2020-05-22 16:53:01 -0700

    fixed bug of missing html

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -6,13 +6,28 @@ var FormView = {
     FormView.$form.on('submit', FormView.handleSubmit);
   },
 
+  //does something for submit event -> we should send message to server -> Parse.create(message);
   handleSubmit: function(event) {
     // Stop the browser from submitting the form
     event.preventDefault();
-    
+
+    var message = {
+    username: App.username,
+    text: $("#message").val(),
+    roomname: 'testRoom'
+    };
+
+    //atempt to send a message to server
+    Parse.create(message); // but how to get message from text bar
+    // var test1 = $("#message").val();
+    // var test2 = document.getElementById("message").value;
+    // console.log(test1);
+    // console.log(test2);
     console.log('click!');
   },
 
+
+  //temp ignored
   setStatus: function(active) {
     var status = active ? 'true' : null;
     FormView.$form.find('input[type=submit]').attr('disabled', status);
--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -2,3 +2,10 @@ var Messages = {
 
 
 };
+
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
